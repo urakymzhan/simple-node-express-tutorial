ce6b5288923060d9b641a2049a14cc9de6a4b591
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-05-21 20:23:49 -0500

    fc

---
+++ database.js
@@ -0,0 +1,20 @@
+const students = [
+  {
+    id: 1,
+    name: "Max",
+  },
+  {
+    id: 2,
+    name: "Meerim",
+  },
+  {
+    id: 3,
+    name: "Mike",
+  },
+  {
+    id: 4,
+    name: "Jazzy",
+  },
+];
+
+module.exports = students;
